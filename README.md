ec8386e90a361abd90136d8fe0dac6b16af7bc65
X1 <chailin6789@gmail.com>
2021-02-26 17:03:51 +0800

    the target could be folder

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-ssh-file-transfer
 - a kit using **paramiko** to realize the file transfer between local windows and ubuntu server
+- the origin target could be a folder, but the nest folders are neglected
--- ssh_transfer.py
+++ ssh_transfer.py
@@ -14,23 +14,37 @@ input_file = "path-to-a-file"
 output_folder = "/path-to-a-folder/"
 
 
-def is_folder(path):
+# verdict whether the path is a folder based on the name
+def is_folder_name(path):
     return True if path[-1] == '\\' or path[-1] == '/' else False
 
 
 def is_ubuntu(path):
-    return True if path[0] == '\\' else False
+    return True if path[0] == '/' else False
 
 
 def get_filename(path):
-    return path.split('\\')[-1] if is_ubuntu(path) else os.path.basename(path)
+    return path.split('/')[-1] if is_ubuntu(path) else os.path.basename(path)
 
 
-# def files_append(file_list, folder):
-#     with paramiko.SSHClient() as ssh:
-#         ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-#         ssh.connect(hostname=hostname, port=port, username=username, password=password)
-#         stdin, stdout, stderr = ssh.exec_command('ls ' + folder)
+def files_append(file_list, input_file, ssh):
+    if is_folder_name(input_file):
+        if is_ubuntu(input_file):
+            stdin, stdout, stderr = ssh.exec_command('ls -F ' + input_file)  # -F: neglect folder of ubuntu
+            out = stdout.readlines()
+            for o in out:
+                temp = input_file + o
+                files_append(file_list, temp[:-1], ssh)  # get rid of the '\n' at ends
+        else:
+            out = os.listdir(input_file)
+            for o in out:
+                temp_path = input_file+o
+                if os.path.isdir(input_file+o):  # neglect folder of win
+                    pass
+                else:
+                    files_append(file_list, temp_path, ssh)
+    else:
+        file_list.append(input_file)
 
 
 def transfer(input_file, output_folder, func='get'):
@@ -42,14 +56,15 @@ def transfer(input_file, output_folder, func='get'):
     with paramiko.SSHClient() as ssh:
         ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
         ssh.connect(hostname=hostname, port=port, username=username, password=password)
+        file_list = []
+        files_append(file_list, input_file, ssh)
         with ssh.open_sftp() as sftp:
-            if func == 'get':
-                sftp.get(input_file, output_folder + get_filename(input_file))
-            elif func == 'put':
-                sftp.put(input_file, output_folder + get_filename(input_file))
-            else:
-                raise ValueError('only \'get\' or \'put\' for param func in transfer()')
+            the_func = sftp.get if func == 'get' else sftp.put
+            print("Transferred => ")
+            for file in file_list:
+                the_func(file, output_folder + get_filename(file))
+                print(file)
 
 
 if __name__ == '__main__':
-    transfer(input_file, output_folder, 'put')
+    transfer(input_file, output_folder, 'get')
